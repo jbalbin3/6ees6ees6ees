ccda52b7197df4a6da7fb385dfa6e73927f3769c
John Balbin <jbalbin3@yahoo.com>
2019-12-31 08:49:14 -0800

    filled in functions

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age, job) {
+    super(age, job);
+    this.age = 10;
+    this.job='make honey';
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot += 1;
+  }
+  giveHoney() {
+    this.honeyPot -= 1;
+  }
 };
